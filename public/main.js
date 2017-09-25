a44255462cfbb04225adc643129562d17d1a5136
Jordan Fred <jcfred88@gmail.com>
2017-09-25 09:54:52 -0600

    final touches

---
+++ public/main.js
@@ -0,0 +1,11 @@
+console.log("js connected");
+
+document.getElementById('homeNav').addEventListener("click", function(){
+  window.location = "./index.html"
+})
+document.getElementById('workNav').addEventListener("click", function(){
+  window.location = "./work.html"
+})
+document.getElementById('contactNav').addEventListener("click", function(){
+  window.location = "./contact.html"
+})
